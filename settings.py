1e09d9158733ba469ee1ad2bbc48344ae72af61d
Olga-Petrova <155875888+Olga-Petrova-QAP174@users.noreply.github.com>
2024-10-06 21:32:35 +0500

    settings.py

--- settings.py
+++ settings.py
@@ -2,11 +2,11 @@ import os
 
 base_url = 'https://b2c.passport.rt.ru/auth/realms/b2c/protocol/openid-connect/auth/?client_id=lk_onlime&redirect_uri=https%3A%2F%2Fmy.rt.ru%2Fauth%2Fssoredirect%2F&response_type=code&auth_type=standard'
 
-email = 'olgap_kurgan@mail.ru'
-phone = '+79080030880'
-login = 'rtkid_1697467366472'
+email = ''
+phone = ''
+login = ''
 
-password = 'Scenic2010'
+password = ''
 
 invalid_phone = 'invalid_phone'
 invalid_email = 'invalid_email@mail.ru'
